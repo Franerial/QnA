c11721bbc6e1886d4f7c965e91b3900679022572
Franerial <brugunt@gmail.com>
2021-11-07 22:01:57 +0300

    User can add several links when create question and answer

--- spec/features/answer/add_links_spec.rb
+++ spec/features/answer/add_links_spec.rb
@@ -7,6 +7,7 @@ feature "User can add links to answer", %q{
 } do
   given(:user) { create(:user) }
   given (:img_url) { "https://www.bl-school.com/blog/wp-content/uploads/2012/09/Koala.jpg" }
+  given (:gem_url) { "https://rubygems.org/gems/cocoon" }
   given(:question) { create(:question) }
 
   scenario "User adds links when create answer", js: true do
@@ -16,10 +17,18 @@ feature "User can add links to answer", %q{
     fill_in "Body", with: "Text text text"
     fill_in "Link name", with: "Koala"
     fill_in "Url", with: img_url
+
+    click_on "Add link"
+    within all(".nested-fields").last do
+      fill_in "Link name", with: "Cocoon gem"
+      fill_in "Url", with: gem_url
+    end
+
     click_on "Create"
 
     within ".answers" do
       expect(page).to have_link "Koala", href: img_url
+      expect(page).to have_link "Cocoon gem", href: gem_url
     end
   end
 end
--- spec/features/question/add_links_spec.rb
+++ spec/features/question/add_links_spec.rb
@@ -7,8 +7,9 @@ feature "User can add links to question", %q{
 } do
   given(:user) { create(:user) }
   given (:img_url) { "https://www.bl-school.com/blog/wp-content/uploads/2012/09/Koala.jpg" }
+  given (:gem_url) { "https://rubygems.org/gems/cocoon" }
 
-  scenario "User adds links when asks question" do
+  scenario "User adds links when asks question", js: true do
     sign_in(user)
     visit new_question_path
 
@@ -16,8 +17,19 @@ feature "User can add links to question", %q{
     fill_in "Body", with: "Text text text"
     fill_in "Link name", with: "Koala"
     fill_in "Url", with: img_url
+
+    click_on "Add link"
+
+    within all(".nested-fields").last do
+      fill_in "Link name", with: "Cocoon gem"
+      fill_in "Url", with: gem_url
+    end
+
     click_on "Ask"
 
-    expect(page).to have_link "Koala", href: img_url
+    within(".question-links") do
+      expect(page).to have_link "Koala", href: img_url
+      expect(page).to have_link "Cocoon gem", href: gem_url
+    end
   end
 end
